c50f866060d761a82b916b9122ece9c497b5f64f
Leksiyy <leksiy197618@gmail.com>
2024-10-09 14:28:28 +0300

    homework 1

---
+++ Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            TcpClient tcpClient = new TcpClient("127.0.0.1", 8080);
+            Console.WriteLine("Подключение успешно");
+
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            Console.WriteLine("Вывести время - 1\nВывести дату - 2\n");
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (input != "1" && input != "2");
+            
+            byte[] data = Encoding.ASCII.GetBytes(input);
+            networkStream.Write(data, 0, data.Length);
+            
+            byte[] buffer = new byte[256];
+            int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+            
+            string response = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine(input == "1" ? $"Время на сервере: {response}" : $"Дата на сервере: {response}");
+            
+            tcpClient.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- NetworkProgramming/Program.cs
+++ NetworkProgramming/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace NetworkProgrammig;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkProgrammig;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        ConnectToServer();
+    }
+    static void ConnectToServer()
+    {
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 11001);
+            Console.WriteLine("Подключено к серверу...");
+ 
+            NetworkStream stream = client.GetStream();
+ 
+            byte[] data = new byte[256];
+            StringBuilder responseData = new StringBuilder();
+            int bytes;
+ 
+ 
+            do
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                responseData.Append(Encoding.ASCII.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable);
+ 
+ 
+            Console.WriteLine("Текущее время с сервера: " + responseData.ToString());
+ 
+ 
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
     }
 }
