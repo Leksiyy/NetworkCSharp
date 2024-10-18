3984987f3514dd95dfa920bc9f6e3d095bd2ab50
Leksiyy <leksiy197618@gmail.com>
2024-10-18 15:44:57 +0300

    homework 5

---
+++ homework5/Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WebClient client = new WebClient();
+        string fileCopy = "../../../copy.png",
+            urlString = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png";
+        client.DownloadFile(urlString, fileCopy);
+    }
+}
